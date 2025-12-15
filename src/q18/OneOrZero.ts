745e12709f79cdc127ba2b1c9ab89b2ef800af16
TEK <68010401@kmitl.ac.th>
2025-12-15 18:14:44 +0700

    7

--- src/q18/OneOrZero.ts
+++ src/q18/OneOrZero.ts
@@ -1,8 +1,8 @@
 const q = parseInt(process.argv[2]);
 for (let i = 0 ; i < q ; i++){
     if( i % 2 ){
-        console.log(`1`.repeat(q));
-    } else{
         console.log(`0`.repeat(q));
+    } else{
+        console.log(`1`.repeat(q));
     }
 }
